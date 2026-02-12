be19b895196f6384d2a4aca1e1ebeef12edbd250
YasminFerrari <161326008+Ferrari65@users.noreply.github.com>
2026-02-12 07:18:18 -0300

    feat:preparação do ambiente de desenvolvimento

---
+++ src/main/java/com/Sacrometrix/Sacrometrix/SacrometrixApplication.java
@@ -0,0 +1,13 @@
+package com.Sacrometrix.Sacrometrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacrometrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacrometrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sacrometrix/Sacrometrix/SacrometrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sacrometrix.Sacrometrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacrometrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
